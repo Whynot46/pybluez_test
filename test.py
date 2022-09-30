c3716affbed443739729e508dc6cb9c9d9014ae7
Whynot46 <83154713+Whynot46@users.noreply.github.com>
2022-09-30 13:45:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,12 @@
 import bluetooth
 
-sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
-
 bd_addr = "B0:FC:36:F5:C3:F0"
-print(bd_addr)
-bd_addr = str(input('Введи адрес, если хочешь его поменять <-- '))
-if bd_addr=='':
-    bd_addr = "B0:FC:36:F5:C3:F0"
-    port = 0x1001
-    sock.connect((bd_addr, port))
-    sock.send("hello!!")
-    sock.close()
-else:
-    port = 0x1001
-    sock.connect((bd_addr, port))
-    sock.send("hello!!")
-    sock.close()
+
+port = 1
+
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((bd_addr, port))
+
+sock.send("I am alive!")
+
+sock.close()
